93e37f5d9760e3ea3c6f9d7737f857514df435de
wayne <wayne.zhan@finsmart.sg>
2025-12-19 16:45:29 +0800

    添加lombok依赖

--- src/main/java/org/example/entity/demo.java
+++ src/main/java/org/example/entity/demo.java
@@ -1,4 +1,9 @@
 package org.example.entity;
 
+import lombok.Data;
+
+@Data
 public class demo {
+    int name;
+    int age;
 }
--- src/main/java/org/example/entity/dto/student.java
+++
@@ -1,4 +0,0 @@
-package org.example.entity.dto;
-
-public class student {
-}
